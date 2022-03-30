d63b3440855a18879cff69288d34f2d6354b68dc
anushkrishnav <anush.venkatakrishna@gmail.com>
2022-03-30 14:13:27 +0530

    feat: Initial commit, Yehh git add .!

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from streamlit import config
+from src.home import Home as home
+from streamlit.script_runner import RerunException, StopException
+
+
+def write_page(page):  # pylint: disable=redefined-outer-name
+    #if page == dash:
+    #    return page.dash_write(name='MSFT', start='2019-06-03', stop='2021-06-03')
+    return page.write()
+PAGES={ 
+    'Home':home,
+
+    }
+    # , 'CryptoCurrency':crypto}
+
+def main():
+    choice=st.sidebar.radio("Click on the pages to explore",tuple(PAGES.keys()))
+
+
+    if choice ==None:
+        write_page(home) 
+    else:
+        page=PAGES[choice]
+        write_page(page)
+
+
+if __name__ == "__main__":
+    st.set_page_config(page_title="NYUAD",layout="wide",page_icon="")
+    main()
---
+++ src/home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import io
+import pandas as pd
+
+class Home:
+    @staticmethod
+    def write():
+        st.write("Everything starts with a Hello world")
